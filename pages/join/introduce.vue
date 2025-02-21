52bf9e5515f32c48c15ed6cf60d810a699e69ffe
KywOoZen <3049176910@qq.com>
2025-02-21 17:36:03 +0800

    add join/introduce | rename login -> register

---
+++ pages/join/introduce.vue
@@ -0,0 +1,77 @@
+<template>
+	<view class="container">
+		<view class="headline">
+			<view class="line1"></view>
+			<view class="line2"></view>
+		</view>
+		
+		<view class="title">{{page.title}}</view>
+		
+		<view class="contents">
+			<view class="content-item" v-for="(item, index) in page.contents" :key="index">
+				<view class="subtitle">{{item.subtitle}}</view>
+				<gradient-line color1="#59C8F5" width="155rpx"></gradient-line>
+				<view class="text">{{item.text}}</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				page: {
+					title: "如何成为入党申请人",
+					contents: [
+						{
+							subtitle: "条件",
+							text: "年满18周岁的中国公民；承认党的纲领和章程；愿意参加党的一个组织并在其中积极工作；愿意执行党的决议；按期缴纳党费。"
+						},
+						{
+							subtitle: "要求",
+							text: "向工作、学习所在单位党组织提出入党申请；没有工作、学习单位或工作、学习单位尚未建立党组织的，向居住地党组织提出入党申请；流动人员还可以向单位所在地党组织或单位主管部门党组织、流动党员党组织提出入党申请。"
+						},
+						{
+							subtitle: "注意",
+							text: "本人提出；书面申请"
+						}
+					]
+				}
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.container {
+	width: 100%;
+	padding: 26rpx 0;
+	.headline {
+		padding: 0 40rpx;
+		.line1 {
+			border-bottom: 8rpx solid #DFDFDF;
+			margin-bottom: 5rpx;
+		}
+		.line2 {
+			border-bottom: 3rpx solid #DFDFDF;
+		}
+	}
+	.title {
+		margin-top: 25rpx;
+		font-size: 40rpx;
+		font-weight: bold;
+		text-align: center;
+	}
+	.contents {
+		padding: 42rpx 44rpx;
+		.content-item {
+			margin-bottom: 60rpx;
+			.text {
+				margin-top: 20rpx;
+			}
+			
+		}
+	}
+}
+</style>
---
+++ pages/register/password.vue
@@ -0,0 +1,139 @@
+<template>
+  <view class="container">
+    <!-- 标题 -->
+    <view class="title">请设置密码</view>
+    <view class="subtitle">密码用于入党E点通登录</view>
+
+    <!-- 输入框 -->
+    <view class="input-container">
+      <text class="label">密码</text>
+      <view class="password-input">
+		<input v-if="showPassword"
+		  type="text"
+		  class="input-field"
+		  v-model="password"
+		/>
+        <input v-else
+          type="password" 
+          placeholder="请输入密码" 
+          class="input-field"
+          v-model="password"
+        />
+        <view class="eye-icon" @click="togglePassword">
+			<image class="eye-open" src="/static/images/login/password/eye-open.png" v-if="showPassword"></image>
+			<image class="eye-close" src="/static/images/login/password/eye-close.png" v-else></image>
+        </view>
+      </view>
+      <text class="hint">需6-12位字符</text>
+    </view>
+
+    <!-- 提交按钮 -->
+    <button class="submit-btn" :class="{active: validPassword}">完成</button>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      password: "",
+      showPassword: false,
+    };
+  },
+  computed: {
+	  validPassword() {
+		  var length = this.password.length;
+		  return length >= 6 && length <= 12;
+	  }
+  },
+  methods: {
+    togglePassword() {
+      this.showPassword = !this.showPassword;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.container {
+  padding: 75rpx 58rpx;
+  background-color: #fff;
+}
+
+.title {
+  font-size: 40rpx;
+  font-weight: bold;
+  color: #000;
+}
+
+.subtitle {
+  font-size: 28rpx;
+  color: #666;
+  margin-top: 10rpx;
+}
+
+.input-container {
+  width: 100%;
+  margin-top: 50rpx;
+
+  .label {
+    font-size: 30rpx;
+    color: #333;
+  }
+
+  .password-input {
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #ddd;
+    margin-top: 10rpx;
+    padding-bottom: 10rpx;
+    position: relative;
+
+    .input-field {
+      flex: 1;
+      font-size: 30rpx;
+      // color: #666;
+      padding: 10rpx 0;
+    }
+
+    .eye-icon {
+      // position: absolute;
+      // right: 10rpx;
+	  width: 35.6rpx;
+	  height: 16.6rpx;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+	  image.eye-close {
+	    width: 35.6rpx;
+	    height: 16.6rpx;
+	  }
+	  image.eye-open {
+	  	width: 43.8rpx;
+	  	height: 31.6rpx;
+	  }
+    }
+
+    
+  }
+
+  .hint {
+    font-size: 24rpx;
+    color: #999;
+    margin-top: 10rpx;
+  }
+}
+
+.submit-btn {
+  width: 100%;
+  height: 90rpx;
+  background-color: #f56c6c;
+  color: #fff;
+  font-size: 32rpx;
+  border-radius: 10rpx;
+  margin-top: 60rpx;
+  &.active {
+	  background-color: #E42417;
+  }
+}
+</style>
