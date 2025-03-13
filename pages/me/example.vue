9e38817a67a95559077b666e3a13598afd2a1a7e
YangYiFan <Coolfan1024@gmail.com>
2025-03-13 23:06:09 +0800

    feat: 材料归档和样例页样式

--- pages/me/example.vue
+++ pages/me/example.vue
@@ -1,20 +1,107 @@
 <template>
-	<view>
-		<!-- 材料范例界面 -->
-		材料范例界面
-	</view>
+    <view class="example-container">
+        <!-- 顶部提示框 -->
+        <view class="tip-box">
+            <text class="tip-text">以下为入党流程中所需材料范例<br />您可按需下载。</text>
+        </view>
+
+        <!-- 材料列表外层容器 -->
+        <view class="materials-container">
+            <view class="materials-list">
+                <view class="material-item" v-for="(item, index) in 10" :key="index">
+
+                </view>
+            </view>
+        </view>
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
-		}
-	}
+export default {
+    data() {
+        return {
+            materials: []  // 预留材料数据数组
+        };
+    }
+}
 </script>
 
 <style lang="scss">
+.example-container {
+    min-height: 100vh;
+    background: linear-gradient(180deg, #0d53d2 0%, #FFFFFF 100%);
+    padding: 30rpx 50rpx;
+    position: relative;
+    z-index: 1;
+}
 
+.tip-box {
+    background-color: #9cb8ec;
+    padding: 40rpx 30rpx;
+    border-radius: 30rpx;
+    margin-bottom: 100rpx;
+    text-align: center;
+
+    .tip-text {
+        color: white;
+        font-size: large;
+        line-height: 1.5;
+    }
+}
+
+.materials-container {
+    background-color: white;
+    border-radius: 16rpx;
+    padding: 30rpx;
+    position: relative;
+    box-shadow: 0 4rpx 20rpx rgba(13, 83, 210, 0.1);
+
+    &::before {
+        content: '';
+        position: absolute;
+        top: -1%;
+        left: 2%;
+        width: 96%;
+        height: 100rpx;
+        background-color: rgba(255, 255, 255, 0.4);
+        border-radius: 16rpx;
+        z-index: -1;
+    }
+
+    &::after {
+        content: '';
+        position: absolute;
+        top: -2%;
+        left: 5%;
+        width: 90%;
+        height: 100rpx;
+        background-color: rgba(255, 255, 255, 0.4);
+        border-radius: 16rpx;
+        z-index: -1;
+    }
+}
+
+.materials-list {
+    padding: 10rpx 0;
+    position: relative;
+    z-index: 3;
+
+    .material-item {
+        background-color: #FFFFFF;
+        border-radius: 12rpx;
+        min-height: 120rpx;
+        margin-bottom: 24rpx;
+        padding: 30rpx;
+        box-shadow: 0 4rpx 12rpx rgba(0, 0, 0, 0.05);
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        border: 1rpx solid black;
+        position: relative;
+
+        &:last-child {
+            margin-bottom: 0;
+        }
+    }
+}
 </style>
